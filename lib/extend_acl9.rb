3c4e8fd9b09e3e6efd90204e68b477449c8c780b
Gianpaulo Soares <jam_pow@hotmail.com>
2011-05-18 10:59:23 -0300

    first commit

---
+++ lib/extend_acl9.rb
@@ -0,0 +1,28 @@
+# ExtendAcl9
+
+module ActionController
+  class Base
+
+    class << self
+
+      LEVELS = {:e => [:index, :show, :new, :edit, :create, :update, :destroy], :l => [:index, :show], :s => [] }
+      # Class methods here
+
+      def controller_name
+        self.name.gsub(/Controller/, '').downcase
+      end
+
+      def autom_permissions
+        access_control do
+          LEVELS.each do |level, permissions|
+            access_name = (controller_name+"_"+level.to_s).to_sym
+            allow access_name, :to => permissions
+          end
+        end
+      end #def autom_permissions
+
+    end #class << self
+  # Instance methods here
+  end
+end
+
